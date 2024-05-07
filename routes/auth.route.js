922afb55637af9577207a23f2328fbfe8ae3b934
fadayopaul <paulfadayo@yahoo.com>
2024-05-07 20:00:42 +0100

    build: :bug: debug

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,15 +1,21 @@
-const express = require('express');
+console.log("Auth routes loaded");
+
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/auth.controller');
-const authValidator = require("../validators/auth.validator")
+const authController = require("../controllers/auth.controller");
+const authValidator = require("../validators/auth.validator");
 
 // User registration
-router.post('/register', authValidator.register, authController.register);
+router.post("/register", authValidator.register, authController.register);
 
 // User verify email
-router.post('/verify-email', authValidator.verifyEmail, authController.verifyEmail);
+router.post(
+  "/verify-email",
+  authValidator.verifyEmail,
+  authController.verifyEmail
+);
 
 // User login
-router.post('/login', authValidator.login, authController.login);
+router.post("/login", authValidator.login, authController.login);
 
 module.exports = router;

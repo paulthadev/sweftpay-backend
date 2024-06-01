1a0bed782b77920089555214c43581ee38652786
fadayopaul <paulfadayo@yahoo.com>
2024-06-01 16:35:56 +0100

    feat: :white_check_mark: Rephrase Email Subject and message text

--- utils/otp.js
+++ utils/otp.js
@@ -1,32 +1,32 @@
-const nodemailer = require('nodemailer');
-const crypto = require('crypto');
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
 const config = require("../config/variables");
 
 const generateOTP = () => {
-    return crypto.randomInt(100000, 999999).toString();
-}
+  return crypto.randomInt(100000, 999999).toString();
+};
 
 const sendEmail = async (email, otp) => {
-    const transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: config.EMAIL,
-            pass: config.PASS,
-        }
-    })
-    const mailOptions = {
-        to: email,
-        from: process.env.MAIL,
-        subject: 'Password Reset OTP',
-        text: `Your OTP for a password reset is: ${otp}\nIt is valid for 10 minutes`
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: config.EMAIL,
+      pass: config.PASS,
+    },
+  });
+  const mailOptions = {
+    to: email,
+    from: process.env.MAIL,
+    subject: "SweftPay Account Registration OTP",
+    text: `Your OTP for your account registration is: ${otp}\nIt is valid for 10 minutes`,
+  };
+  transporter.sendMail(mailOptions, (err) => {
+    if (err) {
+      console.error("Error Sending email", err);
+    } else {
+      console.log("OTP email sent successfully");
     }
-    transporter.sendMail(mailOptions, (err) => {
-        if (err) {
-            console.error('Error Sending email', err)
-        } else {
-            console.log('OTP email sent successfully')
-        }
-    })
-}
+  });
+};
 
-module.exports = {generateOTP, sendEmail}
+module.exports = { generateOTP, sendEmail };
